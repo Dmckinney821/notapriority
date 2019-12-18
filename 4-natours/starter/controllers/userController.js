2b3f66ec64cbcfd842eebbcc7a0ddbda8558f475
Dan McKinney <3dhdaniel@gmail.com>
2019-12-18 11:20:37 -0500

    wasted wednesday

---
+++ 4-natours/starter/controllers/userController.js
@@ -0,0 +1,32 @@
+
+
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is undefined'
+})
+}
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is undefined'
+})
+}
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is undefined'
+})
+}
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is undefined'
+})
+}
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is undefined'
+})
+}
